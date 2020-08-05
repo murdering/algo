71f82e2d0d5b63904b98558caecad0e7569bd198
leodai <murdering@live.cn>
2020-08-05 16:15:49 +0800

    my array implement

---
+++ csharp/05-array/MyArray.cs
@@ -0,0 +1,102 @@
+﻿using System;
+
+namespace algo05_array
+{
+    public class MyArray<T> where T : IComparable<T>
+    {
+        private T[] _arrays;
+        private int _capacity { get; set; }
+        private int _length { get; set; }
+
+        public MyArray(int capacity)
+        {
+            _arrays = new T[capacity];
+            _capacity = capacity;
+        }
+
+        public int Length => _length;
+
+        public T[] Data => _arrays;
+
+        /// <summary>
+        /// 插入元素
+        /// </summary>
+        /// <param name="idx">索引</param>
+        /// <param name="item">元素</param>
+        public void Insert(int idx, T item)
+        {
+            if (idx < 0 || idx >= _capacity)
+            {
+                throw new IndexOutOfRangeException("Index out of range");
+            }
+
+            if (idx <= _length)
+            {
+                for (int i = _length; i > idx; i--)
+                {
+                    _arrays[i] = _arrays[i - 1];
+                }
+
+                _length++;
+            }
+            else
+            {
+                _length = idx;
+            }
+
+            _arrays[idx] = item;
+        }
+
+        /// <summary>
+        /// 删除元素
+        /// </summary>
+        /// <param name="idx"></param>
+        public void Delete(int idx)
+        {
+            if (idx < 0 || idx >= _capacity)
+            {
+                throw new IndexOutOfRangeException("Index out of range");
+            }
+
+            if (_length == 0)
+            {
+                return;
+            }
+
+            if (idx == _length - 1) // 移除最后一个元素
+            {
+                _arrays[idx] = default;
+                _length--;
+            }
+            else if (idx < _length)
+            {
+                for (int i = idx; i < _length - 2; i++)
+                {
+                    _arrays[i] = _arrays[i + 1];
+                }
+
+                // 最后一个元素哨兵处理
+                _arrays[_length - 1] = default;
+                _length--;
+            }
+        }
+
+        /// <summary>
+        /// IndexOf
+        /// </summary>
+        /// <param name="item">元素</param>
+        /// <returns>索引</returns>
+        public int IndexOf(T item)
+        {
+            for (int i = 0; i < _length; i++)
+            {
+                if (_arrays[i].CompareTo(item) == 0)
+                {
+                    return i;
+                }
+            }
+
+            return -1;
+        }
+    }
+}
---
+++ csharp/Tests/_05_array_tests/MyArray_Tests.cs
@@ -0,0 +1,84 @@
+﻿using algo05_array;
+using Xunit;
+
+namespace algo05_array_tests
+{
+    public class MyArray_Tests
+    {
+        public MyArray<int> Arrays { get; set; }
+
+        public MyArray_Tests()
+        {
+        }
+
+        [Fact]
+        public void Can_Insert()
+        {
+            Arrays = new MyArray<int>(10);
+
+            Arrays.Insert(0, 1);
+            Arrays.Insert(1, 2);
+            Arrays.Insert(2, 3);
+            Arrays.Insert(1, 22);
+            Arrays.Insert(7, 7);
+            Arrays.Insert(9, 10);
+            Arrays.Insert(9, 11);
+
+            Assert.Equal(10, Arrays.Length);
+        }
+
+        [Fact]
+        public void Can_Delete()
+        {
+            Arrays = new MyArray<int>(10);
+
+            Arrays.Delete(0);
+            Arrays.Delete(9);
+
+            Arrays.Insert(0, 1);
+            Arrays.Insert(1, 2);
+            Arrays.Insert(2, 3);
+            Arrays.Insert(1, 22);
+            Arrays.Insert(7, 7);
+            Arrays.Insert(9, 10);
+            Arrays.Insert(9, 11);
+
+            Arrays.Delete(0);
+            Arrays.Delete(2);
+            Arrays.Delete(9);
+
+            Assert.Equal(8, Arrays.Length);
+        }
+
+        [Fact]
+        public void Can_IndexOf()
+        {
+            Arrays = new MyArray<int>(10);
+
+            var zeroPis_ZeroVal = Arrays.IndexOf(0);
+            var onePis_OtherValue = Arrays.IndexOf(1);
+
+            Arrays.Insert(0, 1);
+            Arrays.Insert(1, 2);
+            Arrays.Insert(2, 3);
+            Arrays.Insert(1, 22);
+            Arrays.Insert(7, 7);
+            Arrays.Insert(9, 10);
+            Arrays.Insert(9, 11);
+
+            var zeroPis_SomeVal = Arrays.IndexOf(0);
+            var onePis_SomeVal = Arrays.IndexOf(1);
+            var sevebPis_SomeVal = Arrays.IndexOf(7);
+            var tenPis_SomeVal = Arrays.IndexOf(10);
+            var elvPis_SomeVal = Arrays.IndexOf(11);
+
+            Assert.Equal(-1, zeroPis_ZeroVal);
+            Assert.Equal(-1, onePis_OtherValue);
+            Assert.Equal(4, zeroPis_SomeVal);
+            Assert.Equal(0, onePis_SomeVal);
+            Assert.Equal(7, sevebPis_SomeVal);
+            Assert.Equal(-1, tenPis_SomeVal);
+            Assert.Equal(9, elvPis_SomeVal);
+        }
+    }
+}
